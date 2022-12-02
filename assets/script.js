977cdacdeb4808764ad4f476d4d486a9e90de3b1
NathanNguyen <khanhngoc0102@gmail.com>
2022-12-01 20:32:12 -0800

    project commit

---
+++ assets/script.js
@@ -0,0 +1,42 @@
+var ytUrl =
+  "https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyAzn--DVzIXyECqbZJsexiAWQOYzZLf0sA&q=";
+
+var foodName = document.querySelector(".input");
+var btn = document.querySelector(".button");
+var player = document.getElementById("player");
+var recipeText = document.getElementById("embed-containers");
+
+fetch(localStorage.getItem("video"))
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    videoId = data.items[0].id.videoId;
+    ytEndUrl = "https://www.youtube.com/embed/" + videoId;
+    document.getElementById("player").setAttribute("src", ytEndUrl);
+  });
+
+function getInput(event) {
+  event.preventDefault();
+  userInput = foodName.value.replace(/\s/g, "+");
+  ytSearch = ytUrl + userInput + "recipe";
+  setUrl();
+}
+
+function setUrl() {
+  fetch(ytSearch)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      videoId = data.items[0].id.videoId;
+      ytEndUrl = "https://www.youtube.com/embed/" + videoId;
+      console.log(ytEndUrl);
+      document.getElementById("player").setAttribute("src", ytEndUrl);
+      localStorage.setItem("video", ytSearch);
+    });
+}
+
+btn.addEventListener("click", getInput);
+
+// meow
